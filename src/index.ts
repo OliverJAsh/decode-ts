17816252b08b51838265ecf7e2bb363a13e7efc7
Oliver Joseph Ash <oliverjash@gmail.com>
2017-11-10 19:47:56 +0000

    Update lint rules and auto fix

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,21 @@ import * as array from 'fp-ts/lib/Array';
 import * as either from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 import { formatValidationError } from 'io-ts-reporters';
+
 import { ErrorTypes, JsonDecodeError, ParsingErrorError, ValidationErrorsError } from './types';
 
 export type Either<L, A> = either.Either<L, A>;
 
 const typecheck = <A>(a: A) => a;
 
-type jsonParse = (jsonString: string) => Either<ParsingErrorError, any>;
+type jsonParse = (jsonString: string) => Either<ParsingErrorError, {}>;
 const jsonParse: jsonParse = jsonString => (
     either.tryCatch(() => JSON.parse(jsonString))
         .mapLeft(error => new ParsingErrorError(jsonString, error.message))
 );
 
 export type validateType = (
-    <A>(type: t.Type<A>) => (value: any) => Either<ValidationErrorsError, A>
+    <A>(type: t.Type<A>) => (value: {}) => Either<ValidationErrorsError, A>
 );
 export const validateType: validateType = type => value => (
     t.validate(value, type)
@@ -27,7 +28,7 @@ export type jsonDecodeString = (
 );
 export const jsonDecodeString: jsonDecodeString = type => value => (
     // Widen Left generic
-    typecheck<Either<JsonDecodeError, any>>(jsonParse(value)).chain(validateType(type))
+    typecheck<Either<JsonDecodeError, {}>>(jsonParse(value)).chain(validateType(type))
 );
 
 export const formatJsonDecodeError = (error: JsonDecodeError): string[] => {
@@ -38,6 +39,6 @@ export const formatJsonDecodeError = (error: JsonDecodeError): string[] => {
     }
 };
 
-export const reportJsonDecodeError = (result: either.Either<JsonDecodeError, any>): string[] => (
-    result.fold(formatJsonDecodeError, x => [])
+export const reportJsonDecodeError = (result: either.Either<JsonDecodeError, {}>): string[] => (
+    result.fold(formatJsonDecodeError, () => [])
 );
